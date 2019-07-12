0f40ddfc4669a5b5b660d877351c6a03c5a1cff6
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2019-07-12 08:49:19 -0400

    Update terraform config to add new ssm paramater.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 module "iam_user" {
   source = "github.com/cisagov/molecule-packer-travisci-iam-user-tf-module"
 
-  ssm_parameters = ["/github/oauth_token"]
+  ssm_parameters = ["/github/oauth_token", "/cyhy/core/geoip/license_key"]
   user_name      = "test-ansible-role-cyhy-core"
   tags = {
     Team        = "NCATS OIS - Development"
