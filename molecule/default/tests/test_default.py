140f2e242a7eabee1d71761ac2a4a191d2945d86
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-01-04 01:00:35 -0500

    Remove testing for directory we no longer create

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -18,7 +18,7 @@ def test_packages(host, pkg):
     assert pkg in host.pip_package.get_packages()
 
 
-@pytest.mark.parametrize("f", ["/var/local/cyhy/core", "/usr/local/share/GeoIP"])
+@pytest.mark.parametrize("f", ["/usr/local/share/GeoIP"])
 def test_files(host, f):
     """Test that the expected files and directories are present."""
     assert host.file(f).exists
