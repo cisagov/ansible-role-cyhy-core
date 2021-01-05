f8ddb648d2c16b3a29f7710f35083ecefdeecfca
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-01-05 08:51:02 -0500

    Add GeoIP2 downloaded file to testing
    Make sure the dependency is running correctly and the `maxmind_license_key`
    variable is available as expected.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -18,7 +18,14 @@ def test_packages(host, pkg):
     assert pkg in host.pip_package.get_packages()
 
 
-@pytest.mark.parametrize("f", ["/var/local/cyhy/core", "/usr/local/share/GeoIP"])
+@pytest.mark.parametrize(
+    "f",
+    [
+        "/var/local/cyhy/core",
+        "/usr/local/share/GeoIP",
+        "/usr/local/share/GeoIP/GeoIP2-City.tar.gz",
+    ],
+)
 def test_files(host, f):
     """Test that the expected files and directories are present."""
     assert host.file(f).exists
