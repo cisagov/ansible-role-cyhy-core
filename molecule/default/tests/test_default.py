0a895b88aab51f51621f172c2ad2a182cdfe2606
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-01-06 16:55:14 -0500

    Remove testing no longer needed
    Since we are only installing the package with pip now, that is the only test
    still required.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -16,16 +16,3 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_packages(host, pkg):
     """Test that the pip packages were installed."""
     assert pkg in host.pip_package.get_packages()
-
-
-@pytest.mark.parametrize(
-    "f",
-    [
-        "/var/local/cyhy/core",
-        "/usr/local/share/GeoIP",
-        "/usr/local/share/GeoIP/GeoIP2-City.tar.gz",
-    ],
-)
-def test_files(host, f):
-    """Test that the expected files and directories are present."""
-    assert host.file(f).exists
